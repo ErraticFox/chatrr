6ab10e649b53ef5c768240396dc54f6e6367ee41
ErraticFox <erraticfox@gmail.com>
2023-05-22 18:58:10 -0400

    updated PageData type

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,17 +1,18 @@
-declare const { UserSession, UserProfile, UserChatStatus, UserPresence }: import('$lib/models/types');
+import type { UserSession, UserProfile, UserPresence, UserChatStatus } from "$lib/models/types";
 
 declare global {
-	namespace App {
-		interface Locals {
-			userSession: UserSession | undefined;
-		}
-		interface PageData {
-			userSession: UserSession;
-			userProfile: UserProfile;
-			userPresenceStatus: UserPresence;
-			userChatStatus: UserChatStatus;
-		}
-	}
+    namespace App {
+        interface Locals {
+            userSession: UserSession;
+        }
+
+        interface PageData {
+            userSession?: UserSession;
+            userProfile?: UserProfile;
+            userPresenceStatus?: UserPresence;
+            userChatStatus?: UserChatStatus;
+        }
+    }
 }
 
 export {};
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,18 +1,15 @@
-import type { UserPresenceStatus } from "$lib/models/types";
-import { getUserChatStatus, getUserPresenceStatus, getUserProfile } from '$lib/server/chatrr-firebase';
-import type { LayoutServerLoad } from './$types';
+import { getUserChatStatus, getUserPresenceStatus, getUserProfile } from "$lib/server/chatrr-firebase";
+import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ locals }) => {
-	const { userSession } = locals;
+    const { userSession } = locals;
 
-	if (userSession) {
-		const userProfile = await getUserProfile(userSession.uid)
-		const userPresenceStatus = await getUserPresenceStatus(userSession.uid);
-		const userChatStatus = await getUserChatStatus(userSession.uid);
-		if (typeof userChatStatus.timestamp === "number" || typeof userPresenceStatus === "number") {
-			return { userSession, userProfile, userPresenceStatus, userChatStatus };
-		}
-	}
+    if (userSession) {
+        const userProfile = await getUserProfile(userSession.uid);
+        const userPresenceStatus = await getUserPresenceStatus(userSession.uid);
+        const userChatStatus = await getUserChatStatus(userSession.uid);
+        return { userSession, userProfile, userPresenceStatus, userChatStatus };
+    }
 
-	return { userSession };
+    return { userSession };
 };
